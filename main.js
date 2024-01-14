0fcec03f9bfb669382ff0b325563c2f525e5584b
GOLDEN <sergio_dmartinez@soy.sena.edu.co>
2024-01-13 20:14:17 -0500

    Clase lista de productos : HTML a partir de arrays

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const menuCarritoIcom = document.querySelector(".navbar-shoping-card")
 const desktopMenu = document.querySelector(".desktop-menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 const aside = document.querySelector(".product-detail")
+const cardsContiner = document.querySelector(".cards-container")
 
 menuEmail.addEventListener("click", toggleDesktoptMenu)
 menuHamIcom.addEventListener("click", toggleMobileMenu)
@@ -42,5 +43,92 @@ const productList = []
 productList.push({
     name: "Bike",
     price: 120,
-    Image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 })
+productList.push({
+    name: "Pantalla",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+productList.push({
+    name: "Computador",
+    price: 320,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+function renderProducts(arr) {
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute('src', product.image )
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = "$"  + product.price
+    
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+    
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg" )
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContiner.appendChild(productCard)
+    }
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+    
+        const productImg = document.createElement("img")
+        productImg.setAttribute('src', product.image )
+    
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+    
+        const productInfoDiv = document.createElement('div')
+    
+        const productPrice = document.createElement('p')
+        productPrice.innerText = "$"  + product.price
+    
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+    
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+    
+    
+        const productInfoFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', "./icons/bt_add_to_cart.svg" )
+    
+        productInfoFigure.appendChild(productImgCart)
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+    
+        cardsContiner.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
